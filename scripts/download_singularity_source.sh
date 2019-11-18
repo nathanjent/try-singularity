4fcf87b32c18e118af7c2411f790f5194fa30c2d
Nathan Jent <nathanjent@gmail.com>
2019-11-17 22:14:13 -0500

    Update fix download directory

--- scripts/download_singularity_source.sh
+++ scripts/download_singularity_source.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+mkdir -p downloads
+cd downloads
+
 if [ ! -f singularity-${SINGULARITY_VERSION}.tar.gz ]; then
     wget https://github.com/sylabs/singularity/releases/download/v${SINGULARITY_VERSION}/singularity-${SINGULARITY_VERSION}.tar.gz
 fi
